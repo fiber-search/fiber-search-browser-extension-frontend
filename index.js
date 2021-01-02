a58ac4e074c6707a235cfc9c1bcefccaf984b80d
MrWillCom <mr.will.com@outlook.com>
2021-01-02 17:04:54 +0800

    Auto send title after window loaded

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const BACKEND = 'http://localhost:48534';
 var req = new XMLHttpRequest();
 req.open('GET', `${BACKEND}/?type=record&url=${window.location.href}`);
 req.send();
+
+window.addEventListener("load", () => {
+    var req = new XMLHttpRequest();
+    req.open('GET', `${BACKEND}/?type=record-no-increasing&url=${window.location.href}&title=${document.title}`);
+    req.send();
+})
