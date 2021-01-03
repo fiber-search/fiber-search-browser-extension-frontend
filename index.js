23da665eb8e8ea2111ffe576c5d9e3d869e24c2c
MrWillCom <mr.will.com@outlook.com>
2021-01-03 12:23:55 +0800

    Change GET request to POST

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const BACKEND = 'http://localhost:48534';
 
 var req = new XMLHttpRequest();
-req.open('GET', `${BACKEND}/?type=record&url=${window.location.href}`);
+req.open('POST', `${BACKEND}/?type=record&url=${window.location.href}`);
 req.send();
 
 window.addEventListener("load", () => {
     var req = new XMLHttpRequest();
-    req.open('GET', `${BACKEND}/?type=record-no-increasing&url=${window.location.href}&title=${document.title}`);
+    req.open('POST', `${BACKEND}/?type=record-no-increasing&url=${window.location.href}&title=${document.title}`);
     req.send();
 })
